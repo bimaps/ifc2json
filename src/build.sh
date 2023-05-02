33bb94fee05e9a420f2c29d96cbcc3269d950752
Tamu <5020311+Tamu@users.noreply.github.com>
2023-05-02 12:39:31 +0200

    Update build to 6.0

--- src/build.sh
+++ src/build.sh
@@ -4,9 +4,9 @@ dotnet publish --runtime osx-x64 --configuration Release --self-contained false
 dotnet publish --runtime win-x64 --configuration Release --self-contained false
 
 
-cp bin/Release/net5.0/ubuntu.18.04-x64/publish/* ../linux/
-cp bin/Release/net5.0/osx-x64/publish/* ../macos/
-cp bin/Release/net5.0/win-x64/publish/* ../win/
+cp bin/Release/net6.0/ubuntu.18.04-x64/publish/* ../linux/
+cp bin/Release/net6.0/osx-x64/publish/* ../macos/
+cp bin/Release/net6.0/win-x64/publish/* ../win/
 
 echo Everything is published !
 
